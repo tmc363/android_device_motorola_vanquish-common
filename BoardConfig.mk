2700f8045fa06e86edb78d89c6e819cc7a54ddf8
Emerson Pinter <dev@pinter.com.br>
2013-01-06 19:58:28 -0200

    Remove compat flags
    Change-Id: Ie82f5b7e64dea62ce2379ed7441db6cdf45f36de

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,8 +19,6 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 
-TARGET_NO_HW_OVERLAY := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DQCOM_ICS_COMPAT
 BOARD_EGL_CFG := $(LOCAL_PATH)/config/egl.cfg
 
 #bluetooth
