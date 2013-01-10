1e93454f35f54f2e48cc18866e7a3613be566a08
Emerson Pinter <dev@pinter.com.br>
2013-01-10 14:11:38 -0200

    Move display definitions to device
    Change-Id: I3519a41051d21ce6025ebad919faef60a48b0cbd

--- cm.mk
+++ cm.mk
@@ -3,7 +3,3 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
