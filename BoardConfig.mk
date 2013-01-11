8d0cbf4231bc5119714c414c107b7c029f2eb240
Emerson Pinter <dev@pinter.com.br>
2013-01-10 23:18:18 -0200

    Add XT907 to ota-assert
    Change-Id: Ic9f0c9511224ff53bbefc38b560841493add5fa9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
 BOARD_EGL_CFG := $(LOCAL_PATH)/config/egl.cfg
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := xt925,xt926,xt926dev,xt926vzw,vanquish_u,vanquish
+TARGET_OTA_ASSERT_DEVICE := xt925,xt926,xt926dev,xt926vzw,xt907,vanquish_u,vanquish,scorpion_mini
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
