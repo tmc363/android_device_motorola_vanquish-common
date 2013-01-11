1a1e36d787cacdf1e38d19c63c51bd50e22fe5f1
Emerson Pinter <dev@pinter.com.br>
2013-01-10 23:51:51 -0200

    Move all the proprietaries to vendor tree of each device
    Change-Id: I01be08fc41eddbff5e5bab413ad5475bd8f9f4bd

--- device.mk
+++ device.mk
@@ -54,38 +54,9 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/proprietary/wlanprima/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
 	$(LOCAL_PATH)/proprietary/wlanprima/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 
-#touchscreen
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/melfas_45_7_15.fw:system/etc/firmware/melfas_45_7_15.fw
-
 # EGL config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/egl.cfg:system/lib/egl/egl.cfg
-
-#adreno
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/adreno/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
-	$(LOCAL_PATH)/proprietary/adreno/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
-	$(LOCAL_PATH)/proprietary/adreno/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
-	$(LOCAL_PATH)/proprietary/adreno/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
-	$(LOCAL_PATH)/proprietary/adreno/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
-	$(LOCAL_PATH)/proprietary/adreno/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
-	$(LOCAL_PATH)/proprietary/adreno/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
-	$(LOCAL_PATH)/proprietary/adreno/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/libC2D2.so:system/lib/libC2D2.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/libc2d2_a3xx.so:system/lib/libc2d2_a3xx.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/libgsl.so:system/lib/libgsl.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/libsc-a3xx.so:system/lib/libsc-a3xx.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/libOpenVG.so:system/lib/libOpenVG.so \
-	$(LOCAL_PATH)/proprietary/adreno/lib/libOpenCL.so:system/lib/libOpenCL.so \
 
 #safestrap support
 PRODUCT_COPY_FILES += \
