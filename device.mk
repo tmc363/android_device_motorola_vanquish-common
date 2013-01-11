9b6846dd80d6965bf791f94523cd30e996be5bed
Emerson Pinter <dev@pinter.com.br>
2013-01-10 23:17:54 -0200

    Move display density definition to device
    Change-Id: Ia8fa65583be02fde86b4b78a62871ea513582d3d

--- device.mk
+++ device.mk
@@ -125,7 +125,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # QCOM Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=320 \
 	debug.egl.hw=1 \
 	debug.sf.hw=1 \
 	debug.gr.numframebuffers=3 \
